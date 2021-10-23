d69439a41fe79722779699e48c41a17c34282ab9
igor <gresovnik@gmail.com>
2021-10-23 14:14:50 +0200

    renamed bootstrapping updating script

---
+++ bootstrapping/_UpdateBootstrappingScripts.bat
@@ -0,0 +1,18 @@
+
+rem This scripts updates the bootstrapping scripts in the bootstrapping 
+rem directory by copying the most recent versions of the scripts from the 
+rem current repository. 
+rem The working copy of the current repository must be directly contained
+rem in the bootstrapping directory and the appropriate branch or commit
+rem (normally, the master branch) must be checked out.
+rem After running this script, do not forget to commit changes in the 
+rem bootstrapping directory.
+
+@echo off
+
+setlocal
+
+set ScriptDir=
+
+endlocal
+
