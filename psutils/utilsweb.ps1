3d9b67c7cf5d6dcc1f646bd9b62a4a487b7f4703
igor <gresovnik@gmail.com>
2023-11-09 22:49:02 +0100

    utilsweb.ps1: updated detection of the available Wi-Fi networks

--- psutils/utilsweb.ps1
+++ psutils/utilsweb.ps1
@@ -1,8 +1,14 @@
 
 
-#### Wi/Fi utilities
+
+            #########################
+            #                       #
+            #    Wi-Fi utilities    #
+            #                       #
+            #########################
 
 
+######    Wi-Fi connections
 
 
 <#
@@ -89,46 +95,112 @@ function WifiDisplayWeakWarning($RequiredStrengthPercent=80)
 	}
 }
 
-function WifiDisplayAwailableGridView()
+
+
+######    Wi-Fi networks (detectable)
+
+function GetAvailableWifiNetworksStr()
 {
-	$logs=@()
-$date=Get-Date
-$cmd=netsh wlan show networks mode=bssid
-$n=$cmd.Count
-For($i=0;$i -lt $n;$i++)
-{
-If($cmd[$i] -Match '^SSID[^:]+:.(.*)$')
-{
-$ssid=$Matches[1]
-$i++
-$bool=$cmd[$i] -Match 'Type[^:]+:.(.+)$'
-$Type=$Matches[1]
-$i++
-$bool=$cmd[$i] -Match 'Authentication[^:]+:.(.+)$'
-$authent=$Matches[1]
-$i++
-$bool=$cmd[$i] -Match 'Cipher[^:]+:.(.+)$'
-$chiffrement=$Matches[1]
-$i++
-While($cmd[$i] -Match 'BSSID[^:]+:.(.+)$')
-{
-$bssid=$Matches[1]
-$i++
-$bool=$cmd[$i] -Match 'Signal[^:]+:.(.+)$'
-$signal=$Matches[1]
-$i++
-$bool=$cmd[$i] -Match 'Type[^:]+:.(.+)$'
-$radio=$Matches[1]
-$i++
-$bool=$cmd[$i] -Match 'Channel[^:]+:.(.+)$'
-$Channel=$Matches[1]
-$i=$i+2
-$logs+=[PSCustomObject]@{date=$date;ssid=$ssid;Authentication=$authent;Cipher=$chiffrement;bssid=$bssid;signal=$signal;radio=$radio;Channel=$Channel}
+	$cmd=netsh wlan show networks mode=bssid
+	return $cmd
 }
+
+<#
+.Synopsis
+Returns an array of available Wi-Fi networks.
+.Description
+See .Synopsis.
+#>
+function GetAvailableWifiNetworks()
+{
+	$entries=@()
+	$date=Get-Date
+	$cmd=netsh wlan show networks mode=bssid
+	$n=$cmd.Count
+	For($i=0; $i -lt $n; $i++)
+	{
+		If($cmd[$i] -Match '^SSID[^:]+:.(.*)$')
+		{
+			$ssid=$Matches[1]
+			$i++
+			$bool=$cmd[$i] -Match 'Type[^:]+:.(.+)$'
+			$Type=$Matches[1]
+			$i++
+			$bool=$cmd[$i] -Match 'Authentication[^:]+:.(.+)$'
+			$authent=$Matches[1]
+			$i++
+			$bool=$cmd[$i] -Match 'Cipher[^:]+:.(.+)$'
+			$chiffrement=$Matches[1]
+			$i++
+			While($cmd[$i] -Match 'BSSID[^:]+:.(.+)$')
+			{
+				$bssid=$Matches[1]
+				$i++
+				$bool=$cmd[$i] -Match 'Signal[^:]+:.(.+)$'
+				$signal=$Matches[1]
+				$i++
+				$bool=$cmd[$i] -Match 'Type[^:]+:.(.+)$'
+				$radio=$Matches[1]
+				$i++
+				$bool=$cmd[$i] -Match 'Channel[^:]+:.(.+)$'
+				$Channel=$Matches[1]
+				$i=$i+2
+				$entries+=[PSCustomObject]@{ssid=$ssid;Authentication=$authent;Cipher=$chiffrement;bssid=$bssid;signal=$signal;radio=$radio;Channel=$Channel}
+			}
+		}
+	}
+	$cmd=$null
+	$entries 
+	# | Out-String
 }
-}
-$cmd=$null
-$logs|Out-GridView -Title 'Scan Wifi Script'
+
+
+<#
+.Synopsis
+Displays information on available Wi-Fi networks in a grid view.
+.Description
+See .Synopsis.
+#>
+function WifiDisplayAwailableGridView11() 
+{
+	$entries=@()
+	$date=Get-Date
+	$cmd=netsh wlan show networks mode=bssid
+	$n=$cmd.Count
+	For($i=0; $i -lt $n; $i++)
+	{
+		If($cmd[$i] -Match '^SSID[^:]+:.(.*)$')
+		{
+			$ssid=$Matches[1]
+			$i++
+			$bool=$cmd[$i] -Match 'Type[^:]+:.(.+)$'
+			$Type=$Matches[1]
+			$i++
+			$bool=$cmd[$i] -Match 'Authentication[^:]+:.(.+)$'
+			$authent=$Matches[1]
+			$i++
+			$bool=$cmd[$i] -Match 'Cipher[^:]+:.(.+)$'
+			$chiffrement=$Matches[1]
+			$i++
+			While($cmd[$i] -Match 'BSSID[^:]+:.(.+)$')
+			{
+				$bssid=$Matches[1]
+				$i++
+				$bool=$cmd[$i] -Match 'Signal[^:]+:.(.+)$'
+				$signal=$Matches[1]
+				$i++
+				$bool=$cmd[$i] -Match 'Type[^:]+:.(.+)$'
+				$radio=$Matches[1]
+				$i++
+				$bool=$cmd[$i] -Match 'Channel[^:]+:.(.+)$'
+				$Channel=$Matches[1]
+				$i=$i+2
+				$entries+=[PSCustomObject]@{date=$date;ssid=$ssid;Authentication=$authent;Cipher=$chiffrement;bssid=$bssid;signal=$signal;radio=$radio;Channel=$Channel}
+			}
+		}
+	}
+	$cmd=$null
+	$entries | Out-GridView -Title 'Available Wi-Fi networks'
 }
 
 
